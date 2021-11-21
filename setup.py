b258b18a2558d81135fda66be3d8f43b048fdaf0
underwatergrasshopper <93912445+underwatergrasshopper@users.noreply.github.com>
2021-11-21 21:28:49 +0100

    Hotfixed version in setup.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setuptools.setup(
     name                            = "PyUnderGUI",
-    version                         = "0.2.0",
+    version                         = "0.2.1",
     author                          = "underwatergrasshopper",
     author_email                    = "",
     description                     = "Simple GUI library.",
